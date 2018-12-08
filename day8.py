5b02b87c1d3090d6c9eab24e29f7b8826288e0d9
Drew Winstel <drewbrew@users.noreply.github.com>
2018-12-08 09:23:34 -0600

    Add morning-after clarifications
    1. Extra comments
    2. Delete unused arguments
    3. Use proper `if __name__ == '__main__'` syntax so this can be
       imported

--- day8.py
+++ day8.py
@@ -13,15 +13,15 @@ real_inputs = [int(i) for i in real_source.split(' ')]
 # (metadata_total, remaining list)
 
 
-def parse_node_list(node_list, recursion_depth=0):
-    """convert a node into metadata, remaining list"""
+def parse_node_list(node_list):
+    """convert a node into metadata sum, remaining list"""
     number_of_children = node_list.pop(0)
     metadata_entries = node_list.pop(0)
     metadata_total = 0
+    # make a copy of the list so we don't disturb the caller's version
     remaining_list = node_list[:]
     while number_of_children > 0:
-        metadata_from_kid, remaining_list = parse_node_list(
-            remaining_list, recursion_depth + 1)
+        metadata_from_kid, remaining_list = parse_node_list(remaining_list)
         metadata_total += metadata_from_kid
         number_of_children -= 1
     while metadata_entries > 0:
@@ -31,8 +31,10 @@ def parse_node_list(node_list, recursion_depth=0):
     return metadata_total, remaining_list
 
 
-def calc_node_value(node_list, recursion_depth=0):
+def calc_node_value(node_list):
     """count node value for a node"""
+    # Yeah, I don't like the amount of duplicated code either, but I wasn't
+    # going for long-term maintainability here.
     number_of_children = node_list.pop(0)
     children_processed = 0
     metadata_entries = node_list.pop(0)
@@ -44,28 +46,40 @@ def calc_node_value(node_list, recursion_depth=0):
             metadata_total += remaining_list.pop(0)
             metadata_entries -= 1
         return metadata_total, remaining_list
+    # use a dict rather than a list so we don't have to worry about off-by-one
+    # errors. It's not quite as memory-efficient, but it gets the job done
+    # for this puzzle case.
     child_metadata = {}
     while number_of_children > 0:
         children_processed += 1
-        metadata_from_kid, remaining_list = calc_node_value(
-            remaining_list, recursion_depth + 1)
+        metadata_from_kid, remaining_list = calc_node_value(remaining_list)
         child_metadata[children_processed] = metadata_from_kid
         number_of_children -= 1
     while metadata_entries > 0:
         metadata_value = remaining_list.pop(0)
         try:
+            # in part 1 we added this value to a running total
+            # whereas in this case, we need to look up the referenced child's
+            # node value (which is the sum of metadata *if* it has no children
+            # but is otherwise the sum of referenced children's metadata)
             metadata_total += child_metadata[metadata_value]
         except KeyError:
             # child doesn't exist. Don't care
             pass
         metadata_entries -= 1
 
+    # pass whatever data we haven't parsed back to the caller, which can decide
+    # whether that data is another node or just metadata
     return metadata_total, remaining_list
 
 
-parsed_test = parse_node_list(test_inputs[:])
-assert parsed_test[0] == 138, parsed_test
-print('Day 8, part 1 solution', parse_node_list(real_inputs[:])[0])
-test_value = calc_node_value(test_inputs)
-assert test_value[0] == 66, test_value
-print('Day 8, part 2 solution', calc_node_value(real_inputs)[0])
+if __name__ == '__main__':
+    # because each function returns a tuple and the solution only cares about
+    # the total, we can ignore the second part, which is guaranteed to be an
+    # empty list
+    parsed_test = parse_node_list(test_inputs[:])
+    assert parsed_test[0] == 138, parsed_test
+    test_value = calc_node_value(test_inputs)
+    assert test_value[0] == 66, test_value
+    print('Day 8, part 1 solution', parse_node_list(real_inputs[:])[0])
+    print('Day 8, part 2 solution', calc_node_value(real_inputs)[0])
